8c931eec83bf9250ef94f8cf3bb1c377d2b48b5f
Vibhor Khanna <vibkhanna@deloitte.com>
2022-09-24 15:29:10 +0530

    minor ui fixes

--- pehchaan-kaun-ui/src/components/Header/Header.jsx
+++ pehchaan-kaun-ui/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.scss";
 import Dlogo from "../../img/dlogo.svg";
-
+import chaupLogo from "../../img/ChaupaalLogo.png";
 const Header = () => {
   return (
     <div className="header">
-      <img src={Dlogo} alt="Deloitte" />
+      <img className="dlogo" src={Dlogo} alt="Deloitte" />
+      <span>? Pehchaan Kaun ¿</span>
+      <img className="clogo" src={chaupLogo} alt="Chaupaal" />
     </div>
   );
 };
